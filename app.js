0e77a906f36f7603c73a36749944699d17f60ba7
Owner <00009951wiut@gmail.com>
2021-04-07 19:36:59 +0500

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+
+app.set('view engine', 'pug')
+
+// http://localhost:8000
+app.get('/', (req, res) => {
+    res.render('home')
+
+})
+
+
+app.listen(PORT, (err) =>{
+    if (err) throw err
+
+    console.log('Running' + PORT)
+})
