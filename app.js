b04c06c07cb014988512ee3097c0a810e5c8572b
Owner <00009951wiut@gmail.com>
2021-04-07 23:02:50 +0500

    Stage 2

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+
+// third party libs
 const express = require('express')
 const app = express()
+
+// node libs
+const fs = require('fs')
 const PORT = 8000
 
 
 app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
-// http://localhost:8000
+//http://localhost:8000
 app.get('/', (req, res) => {
     res.render('home')
 
 })
 
+app.post('/add', (req, res) => {
+   const formData = req.body
 
-app.listen(PORT, (err) =>{
+   if (formData.todo.trim() == '') {
+       res.render('home', { error: true })
+   } else {
+       fs.readFile('.data/todos.json', (err, data))
+   } 
+})
+
+app.listen(PORT, (err) => {
     if (err) throw err
 
-    console.log('Running' + PORT)
+    console.log(`This app is running on port ${ PORT }`)
 })
